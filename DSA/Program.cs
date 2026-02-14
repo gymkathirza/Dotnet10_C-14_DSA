05488ab7737725024c9a1b5288c79d3d3b44d8e0
gymkathirza <gymkathirza@gmail.com>
2026-02-14 04:37:06 +0000

    minor fix and code comment

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,8 +1,8 @@
 ﻿using DSA;
-// See https://aka.ms/new-console-template for more information
 
+// Array and list in C# 10 with collection expression and array indexing.
 int[] arr = [0,1,2,3,4,5,6,7,8,9];    
-int[] list = [..arr, 10,11,12,13,14,15];
+List<int> list = [..arr, 10,11,12,13,14,15];
 arr[3] = 15;
 Console.WriteLine($"Array elements: {string.Join(", ", arr)}");
 Console.WriteLine($"List elements: {string.Join(", ", list)}");
